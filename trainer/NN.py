d93e0b26af7dadab781289639d21c1306c86efe8
Marcin Wojnarowski <marcin.wojnarowski@ibm.com>
2018-09-01 14:07:29 +0200

    implemented biases

--- trainer/NN.py
+++ trainer/NN.py
@@ -28,7 +28,13 @@ class NN:
             *[[[random.random() for x in range(thicknessLength)]
                for x in range(thicknessLength)] for x in range(hiddenLayersLength-1)],
             [[random.random() for x in range(thicknessLength)]
-             for x in range(outputsLength)],
+             for x in range(outputsLength)]
+        ]
+
+        self.biases = [
+            *[[random.random() for x in range(thicknessLength)]
+              for x in range(hiddenLayersLength)],
+            [random.random() for x in range(outputsLength)]
         ]
 
     def guess(self, inputs):
@@ -42,20 +48,20 @@ class NN:
         sums = [0 for x in range(self.lengths.thickness)]
         for i, node in enumerate(self.weights[0]):
             sums[i] = sum(input*weight for input, weight in zip(inputs, node))
-            sums[i] = self.activation(sums[i])
+            sums[i] = self.activation(sums[i] + self.biases[0][i])
 
         # hidden
         for n in range(self.lengths.hiddenLayers-1):
             temp = [0 for x in range(self.lengths.thickness)]
-            for i, node in enumerate(self.weights[1+n]):
+            for i, node in enumerate(self.weights[n+1]):
                 temp[i] = sum(sum_*weight for sum_, weight in zip(sums, node))
-                temp[i] = self.activation(temp[i])
+                temp[i] = self.activation(temp[i] + self.biases[n+1][i])
             sums = [*temp]
 
         # output
         results = [0 for x in range(self.lengths.outputs)]
         for i, node in enumerate(self.weights[len(self.weights)-1]):
-            results[i] = sum(sum_*weight for sum_, weight in zip(sums, node))
+            results[i] = sum(sum_*weight for sum_, weight in zip(sums, node)) + self.biases[len(self.biases)-1][i]
 
         return results.index(max(*results))
 
@@ -71,8 +77,15 @@ class NN:
 
     def save(self, path):
         with open(path, 'w') as f:
-            json.dump(self.weights, f, indent=4)
+            obj = {
+                'weights': self.weights,
+                'biases': self.biases
+            }
+            json.dump(obj, f, indent=4)
 
     def load(self, path):
         with open(path) as f:
-            self.weights = json.load(f)
+            obj = json.load(f)
+            self.weights = obj['weights']
+            self.biases = obj['biases']
+            print(self.biases[2][9])
